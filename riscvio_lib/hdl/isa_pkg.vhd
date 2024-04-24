61f5be43ad3abf400dcc81cc1320e4bac0228377
Robin Lux <st167825@stud.uni-stuttgart.de>
2024-04-24 16:47:07 +0200

    updated isa pkg

--- riscvio_lib/hdl/isa_pkg.vhd
+++ riscvio_lib/hdl/isa_pkg.vhd
@@ -10,8 +10,14 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 PACKAGE isa IS
-    constant WORD_SIZE: natural := 64;
+    constant DWORD_SIZE: natural := 64;
+    constant WORD_SIZE: natural := 32; 
+    constant HALF_WORD_SIZE: natural := 16;
+    constant BYTE_SIZE: natural := 8;
     constant INSTRUCTION_SIZE: positive := 4;
 
     subtype wordT is std_logic_vector(WORD_SIZE - 1 downto 0);
+    subtype double_wordT is std_logic_vector(DWORD_SIZE - 1 downto 0);
+    subtype half_double_wordT is std_logic_vector(HALF_WORD_SIZE - 1 downto 0);
+    subtype byteT is std_logic_vector(BYTE_SIZE - 1 downto 0);
 END isa;
